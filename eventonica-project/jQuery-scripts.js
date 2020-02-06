64bef01f83fa9260b526dbe0458304acbf2bc171
ArielRenée <rios94.a@gmail.com>
2020-02-06 13:51:03 -0800

    Created Navigation Bar using HTML + JS + CSS

--- eventonica-project/jQuery-scripts.js
+++ eventonica-project/jQuery-scripts.js
@@ -1,4 +1,17 @@
 $(document).ready( () => {
     const eventRecommender = new EventRecommender(); 
     // Your code here
+    // Default Users
+    eventRecommender.addUser();
+    
+    // Default Events
+    eventRecommender.addEvent();
+
+    let mainNav = document.getElementById('js-menu');
+    let navBarToggle = document.getElementById('js-navbar-toggle');
+    
+    navBarToggle.addEventListener('click', function () {
+      mainNav.classList.toggle('active');
+    });
+
 });
