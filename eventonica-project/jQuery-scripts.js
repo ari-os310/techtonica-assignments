a94c6db8dd8d19e3d37f16e832403ccc61a57d69
ArielRenée <rios94.a@gmail.com>
2020-02-13 17:32:35 -0800

    Added jQuery for displaying Users

--- eventonica-project/jQuery-scripts.js
+++ eventonica-project/jQuery-scripts.js
@@ -1,13 +1,26 @@
 $(document).ready( () => {
     const eventRecommender = new EventRecommender(); 
     // Your code here
-    // Default Users
-    eventRecommender.addUser("XELA", "323");
+                          // Default Users
+    eventRecommender.addUser("XELA", 323);
+    eventRecommender.addUser("Nkiru", 515);
+    eventRecommender.addUser("RGBM", 333);
     
-    // Default Events
-    eventRecommender.addEvent();
+                        // Default Events
+    eventRecommender.addEvent("Emo Fantasy", "2020-2-20", "Music", 022);
 
-    // NavBar usability
+                        // display Users
+            displayUsers();
+  
+    function displayUsers() {
+      let defaultUser = "";
+        for (let user of eventRecommender.users) {
+          defaultUser += `<li>${user.name}</li>`;
+      }
+      $("#all-users").html(defaultUser);
+    }
+
+                      // NavBar usability
     let mainNav = document.getElementById('js-menu');
     let navBarToggle = document.getElementById('js-navbar-toggle');
     
@@ -15,7 +28,4 @@ $(document).ready( () => {
       mainNav.classList.toggle('active');
     });
 
-    // $("#delete-event input[type='submit']").click(function(e) {
-    //     //this is to prevent the page from reloading
-    //     e.preventDefault();
 });
